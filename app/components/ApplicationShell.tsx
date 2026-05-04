b909eb5d5dda33405bde2118f5c97fc621452c50
hecharli <charhern1676@gmail.com>
2026-05-04 03:08:02 -0400

    Project init

---
+++ app/components/ApplicationShell.tsx
@@ -0,0 +1,13 @@
+/* 
+The main idea of this is to
+1. make a website that requires an authorized for access (auth) -> handle in <ApplicationShell>
+2. Allow user to make submissions of jobs they are applying for -> handle in separate comp
+3. on submit send that to a database (supabase) and track it there -< handle in 3rd separate com
+*/
+
+// DOM root entry "top level component"
+export default function ApplicationShell(){
+    return(
+        <>test 2</>
+    );
+}
